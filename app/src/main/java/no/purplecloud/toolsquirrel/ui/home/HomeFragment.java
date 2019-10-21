bdb126e696e7453969050ee909567e6f2d7a2331
Unknown <aronmar@live.no>
2019-10-21 18:33:58 +0200

    Change home_fragment from root RecyclerView to RelativeLayout.
    So we can get the recyclerview list as a "component" inside the home fragment instead of retrieving the whole fragment as a recycler view

--- app/src/main/java/no/purplecloud/toolsquirrel/ui/home/HomeFragment.java
+++ app/src/main/java/no/purplecloud/toolsquirrel/ui/home/HomeFragment.java
@@ -25,15 +25,20 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_home, container, false);
+        // The root view where the recycler view is located (home fragment)
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        // The recycler view
+        RecyclerView recyclerView = rootView.findViewById(R.id.home_recycler_view);
+        // Linear layout
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(recyclerView.getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
+        // Divider decoration to set lines between each recycled view
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), linearLayoutManager.getOrientation());
         recyclerView.addItemDecoration(dividerItemDecoration);
         this.homeViewModel = ViewModelProviders.of(this.getActivity()).get(HomeViewModel.class);
         this.homeViewModel.getTools().observe(this, tools ->
                 recyclerView.setAdapter(new ToolListRecyclerViewAdapter(tools)));
-        return recyclerView;
+        return rootView;
     }
 
 }
