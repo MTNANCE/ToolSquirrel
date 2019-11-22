23ec0f7f0a892c39168fc54b8a3c6f934ed7cf50
Tiramisu <libanbn@stud.ntnu.no>
2019-11-22 15:47:24 +0100

    Implement image uploading when adding new project

--- app/src/main/java/no/purplecloud/toolsquirrel/ui/project/NewProjectFragment.java
+++ app/src/main/java/no/purplecloud/toolsquirrel/ui/project/NewProjectFragment.java
@@ -1,26 +1,27 @@
 package no.purplecloud.toolsquirrel.ui.project;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
+import java.io.ByteArrayOutputStream;
 
 import no.purplecloud.toolsquirrel.Endpoints;
 import no.purplecloud.toolsquirrel.R;
@@ -33,6 +34,8 @@ public class NewProjectFragment extends Fragment {
     private TextView projectLocation;
     private TextView projectStatus;
     private Button submitBtn;
+    private Button uploadImageBtn;
+    private ImageView uploadedImg;
 
     @Nullable
     @Override
@@ -43,12 +46,23 @@ public class NewProjectFragment extends Fragment {
         this.projectLocation = root.findViewById(R.id.new_project_location);
         this.projectStatus = root.findViewById(R.id.new_project_status);
         this.submitBtn = root.findViewById(R.id.new_project_button);
+
+        this.uploadImageBtn = root.findViewById(R.id.uploadImageBtn);
+        this.uploadedImg = root.findViewById(R.id.uploadedImageView);
         return root;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        // Set "select image" event when pressing the upload image button
+        uploadImageBtn.setOnClickListener(l -> {
+            startActivityForResult(new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI),
+                    3);
+        });
+
         this.submitBtn.setOnClickListener(event -> {
             String mProjectName = this.projectName.getText().toString();
             String mProjectDesc = this.projectDesc.getText().toString();
@@ -71,11 +85,29 @@ public class NewProjectFragment extends Fragment {
     }
 
     public void newProject(String name, String desc, String location) {
+        // Check if client has uploaded an image
+        if (uploadedImg.getDrawable() instanceof AdaptiveIconDrawable) {
+            this.projectStatus.setText("Please select an image.");
+            this.projectStatus.setTextColor(Color.parseColor("#e01919"));
+            return;
+        }
+
+        // Get iamge as bitmap
+        Bitmap bitmap = ((BitmapDrawable) uploadedImg.getDrawable()).getBitmap();
+
+        // Create an output stream and feed it the content of the image
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+
+        // Get the content as bytes (will be used to send to API)
+        byte[] imgBytes = baos.toByteArray();
+
         try {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("name", name);
             jsonObject.put("desc", desc);
             jsonObject.put("location", location);
+            jsonObject.put("image", imgBytes);
 
             VolleySingleton.getInstance(getContext()).postRequest(Endpoints.URL + "/addNewProject", jsonObject,
                     response -> {
@@ -88,6 +120,18 @@ public class NewProjectFragment extends Fragment {
             );
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // When the client has chosen a picture, update the imageview
+        if (data != null) {
+            if (resultCode == Activity.RESULT_OK) {
+                uploadedImg.setImageURI(data.getData());
+            }
         }
     }
 }
