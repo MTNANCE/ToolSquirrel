3c68ee89e1e51e0a2ac092220a525be9172f57ea
Tiramisu <libanbn@stud.ntnu.no>
2019-11-22 19:43:55 +0100

    Add available tools & only show unique tools on Home

--- app/src/main/java/no/purplecloud/toolsquirrel/ui/home/ToolDetailsFragment.java
+++ app/src/main/java/no/purplecloud/toolsquirrel/ui/home/ToolDetailsFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,13 +17,20 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.Arrays;
+import java.util.List;
+
+import no.purplecloud.toolsquirrel.Endpoints;
 import no.purplecloud.toolsquirrel.R;
+import no.purplecloud.toolsquirrel.domain.Tool;
+import no.purplecloud.toolsquirrel.network.VolleySingleton;
 
 public class ToolDetailsFragment extends Fragment {
 
     private ImageView image;
     private TextView title;
     private TextView description;
+    private TableLayout tableLayout;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +41,7 @@ public class ToolDetailsFragment extends Fragment {
             ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(tool.getToolName());
             this.title.setText(tool.getToolName());
             this.description.setText(tool.getToolDesc());
+            getAllDuplicateTools(tool.getToolName());
         });
     }
 
@@ -42,6 +52,36 @@ public class ToolDetailsFragment extends Fragment {
         this.image = result.findViewById(R.id.tool_details_image);
         this.title = result.findViewById(R.id.tool_details_tile);
         this.description = result.findViewById(R.id.tool_details_desc);
+        this.tableLayout = result.findViewById(R.id.tool_availability_table);
         return result;
+    }
+
+    private void getAllDuplicateTools(String toolName) {
+        VolleySingleton.getInstance(getContext())
+                .searchPostRequest(Endpoints.URL + "/getToolWithStatus/", toolName, "tool",
+                        this::formatToolAvailabilityTable);
+    }
+
+    private void formatToolAvailabilityTable(List<Tool> tools) {
+        tools.forEach(tool -> {
+            TableRow tr = new TableRow(getContext());
+
+            TextView txtId = new TextView(getContext());
+            TextView txtStatus = new TextView(getContext());
+            TextView txtLocation = new TextView(getContext());
+
+            txtId.setText("#".concat(String.valueOf(tool.getId())));
+            txtStatus.setText("Not impl.ed");
+            txtLocation.setText(tool.getToolLocation());
+
+            List<TextView> textViews = Arrays.asList(txtId, txtStatus, txtLocation);
+
+            textViews.forEach(textView -> {
+                textView.setTextSize(14);
+                tr.addView(textView);
+            });
+
+            tableLayout.addView(tr);
+        });
     }
 }
